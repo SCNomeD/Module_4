d782340bf2e8b4ea810e0455f796405f890c6ab7
SCNomeD <vladiiimir1991@gmail.com>
2022-02-13 17:25:56 +0300

    циклы while и do while совмещены

--- Module_4/Program.cs
+++ Module_4/Program.cs
@@ -6,8 +6,48 @@ namespace Module_4
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Цикл do while");
+            Console.WriteLine("Цикл while");
+            int k = 0;
 
+            while (k < 1)
+            {
+                Console.WriteLine(k);
+
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+                k++;
+            }
+
+            Console.WriteLine("Цикл do while");
             int t = 0;
 
             do
@@ -45,11 +85,9 @@ namespace Module_4
                         Console.WriteLine("Your color is yellow!");
                         break;
                 }
-
                 t++;
             }
-
-            while (t > 0);
+            while (t < 1);
 
             Console.ReadKey();
         }
