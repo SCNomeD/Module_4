f90572f1af8fdaa724b3b6b640e527efe7b7c16b
SCNomeD <vladiiimir1991@gmail.com>
2022-02-15 21:48:45 +0300

    Unit 4.3.12

--- Module_4/Program.cs
+++ Module_4/Program.cs
@@ -6,14 +6,47 @@ namespace Module_4
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Введите своё имя");
+            //int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }};
 
-			var name = Console.ReadLine();
+            //Первый
+            //Console.Write("Количество строк: ");
+            //Console.WriteLine(array.GetUpperBound(0) + 1 + " ");
 
-			for (int i = name.Length - 1; i >=0; i--)
-            {
-                Console.WriteLine(name[i]);
-            }
+            //Console.Write("Количество колонок: ");
+            //Console.WriteLine(array.GetUpperBound(1) + 1 + " ");
+
+            //Второй
+            //for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            //{
+            //    for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+            //    Console.Write(array[k, i] + " ");
+
+            //    Console.WriteLine();
+            //}
+
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+
+			Console.WriteLine(arr.Length);
+
+			int temp;
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				for (int j = i + 1; j < arr.Length; j++)
+				{
+					if (arr[i] > arr[j])
+					{
+						temp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = temp;
+					}
+				}
+			}
+
+			foreach (var item in arr)
+			{
+				Console.Write(item + " ");
+			}
 
 			Console.ReadKey();
 		}
