8c5ef7f2274a49fed3e343c2fbeb95288320cc7b
SCNomeD <vladiiimir1991@gmail.com>
2022-02-15 22:20:55 +0300

    unit 4.3.15

--- Module_4/Program.cs
+++ Module_4/Program.cs
@@ -24,18 +24,44 @@ namespace Module_4
             //    Console.WriteLine();
             //}
 
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            //var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-			int sum = 0;
+            //int sum = 0;
 
-			for (int i = 0; i < arr.Length; i++)
-			{
-                sum += arr[i];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //  sum += arr[i];
+            //}
+
+            //Console.WriteLine(sum);
+
+            //Console.ReadKey();
+
+            int[][] array = new int[3][];
+
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+            foreach (var num in array)
+            {
+                foreach (var item in num)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+
+            int[] array_2 = { -1, -2, 4, 8, -5 };
+
+            int sum = 0;
+
+            for (int i = 0; i < array_2.Length; i++)
+            {
+                if (array_2[i] > 0)
+                    sum += array_2[i];
             }
 
             Console.WriteLine(sum);
-
-            Console.ReadKey();
-		}
+        }
     }
 }
