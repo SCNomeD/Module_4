76c1b57db3f59593e337619a2c33131cc879944b
SCNomeD <vladiiimir1991@gmail.com>
2022-02-17 22:26:56 +0300

    Практикум

--- Module_4/Program.cs
+++ Module_4/Program.cs
@@ -163,23 +163,61 @@ namespace Module_4
             //Console.WriteLine(sum);
 
             //Кортежи
-            (string Name, string Type, double Age, int NameCount) Pet;
+            //(string Name, string Type, double Age, int NameCount) Pet;
 
-            Console.Write("Введите имя питомца: ");
-            Pet.Item1 = Console.ReadLine();
+            //Console.Write("Введите имя питомца: ");
+            //Pet.Item1 = Console.ReadLine();
 
-            Console.Write("Введите тип питомца (собака, кошка и т.д.): ");
-            Pet.Item2 = Console.ReadLine();
+            //Console.Write("Введите тип питомца (собака, кошка и т.д.): ");
+            //Pet.Item2 = Console.ReadLine();
 
-            Console.Write("Введите возраст питомца: ");
-            Pet.Item3 = double.Parse(Console.ReadLine());
+            //Console.Write("Введите возраст питомца: ");
+            //Pet.Item3 = double.Parse(Console.ReadLine());
 
-            Pet.Item4 = Pet.Item1.Length;
+            //Pet.Item4 = Pet.Item1.Length;
 
-            Console.WriteLine("Вашего питомца зовут: {0}", Pet.Item1);
-            Console.WriteLine("Тип Вашего питомца: {0}", Pet.Item2);
-            Console.WriteLine("Возраст Вашего питомца: {0}", Pet.Item3);
-            Console.WriteLine("Длина имени Вашего питомца: {0}", Pet.Item4);
+            //Console.WriteLine("Вашего питомца зовут: {0}", Pet.Item1);
+            //Console.WriteLine("Тип Вашего питомца: {0}", Pet.Item2);
+            //Console.WriteLine("Возраст Вашего питомца: {0}", Pet.Item3);
+            //Console.WriteLine("Длина имени Вашего питомца: {0}", Pet.Item4);
+
+            //Практикум
+
+            (string Name, string SName, string Login, int LoginLength, bool Pet, double Age, string[] FavColors) User;
+
+            for (int j = 0; j < 3; j++)
+            {
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию");
+                User.SName = Console.ReadLine();
+
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                var ans = Console.ReadLine();
+                if (ans == "Да")
+                {
+                    User.Pet = true;
+                }
+                else
+                {
+                    User.Pet = false;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+                User.Age = double.Parse(Console.ReadLine());
+
+                User.FavColors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+                for (int i = 0; i < User.FavColors.Length; i++)
+                {
+                    User.FavColors[i] = Console.ReadLine();
+                }
+            }
 
             Console.ReadKey();
         }
