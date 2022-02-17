6520bb78d12641a9353c390b9c2880f971e442fd
SCNomeD <vladiiimir1991@gmail.com>
2022-02-17 21:02:59 +0300

    сортировка + суммирование

--- Module_4/Program.cs
+++ Module_4/Program.cs
@@ -2,10 +2,10 @@
 
 namespace Module_4
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             //int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }};
 
             //Первый
@@ -79,26 +79,88 @@ namespace Module_4
 
             //Console.WriteLine(temp);
 
-            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            //int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            //int temp;
+
+            //for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            //{
+            //    for (int j = 0; j <= arr.GetUpperBound(1); j++)
+            //        for (int k = j+1; k <= arr.GetUpperBound(1); k++)
+            //            if (arr[i,j] > arr [i, k])
+            //            {
+            //                temp = arr[i, k];
+            //                arr[i,k] = arr[i, j];
+            //                arr[i,j] = temp;
+            //            }
+            //}
+
+            //foreach (var item in arr)
+            //{
+            //    Console.Write(item);
+            //}
+
+            //int[,] array = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+
+            //Console.Write("Количество строк: ");
+            //Console.WriteLine(array.GetUpperBound(0) + 1);
+
+            //Console.Write("Количество колонок: ");
+            //Console.WriteLine(array.GetUpperBound(1) + 1);
+
+            //for (int j = 0; j < array.GetUpperBound(0) + 1; j++)
+            //{
+            //    for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+            //        Console.Write(array[j, k] + " ");
+
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine();
+
+            //for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
+            //{
+            //    for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+            //        Console.Write(array[k, j] + " ");
+
+            //    Console.WriteLine();
+            //}
+
+
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
             int temp;
 
-            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j <= arr.GetUpperBound(1); j++)
-                    for (int k = j+1; k <= arr.GetUpperBound(1); k++)
-                        if (arr[i,j] > arr [i, k])
-                        {
-                            temp = arr[i, k];
-                            arr[i,k] = arr[i, j];
-                            arr[i,j] = temp;
-                        }
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
             }
 
             foreach (var item in arr)
             {
                 Console.Write(item);
             }
+
+            Console.WriteLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
         }
     }
 }
